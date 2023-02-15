1a82b558563d11aa431cd75780e831cf4a040146
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-02-15 21:15:07 +0100

    M

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from project import create_app, db
 from project.models.User import User
 from project.models.Therapist import Therapist
 from project.models.Admin import Admin
+from project.models.Appointment import Appointment
+from project.models.City import City
 
 
 app = create_app()
